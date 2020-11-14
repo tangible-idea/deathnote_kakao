114860de641d5b284c5ef8c9af83ca3fab823dc1
Mark Choi <fantasysa@gmail.com>
2020-11-14 15:49:18 +0900

    Set first and latest chat.

--- sagas/conversation.js
+++ sagas/conversation.js
@@ -12,10 +12,31 @@ const tag = "sagas/conversation: "
 class MyUser {
     bubblecount= 0;
     textcount= 0;
-    firstchat= 0;
-    latestchat= 0;
+    firstchat= new Date();
+    latestchat= new Date();
     constructor(name) {
       this.name = name;
+    }
+
+    setFirstChat(date) {
+        var date= new Date(date);
+        //var msDate = Date.UTC(a.getFullYear(), a.getMonth()+1, a.getDate());
+        this.firstchat = date;
+    }
+    setLateDateAsLatestChat(date1, date2){
+        var a = new Date(date1);
+        var b = new Date(date2);
+        var msDateA = Date.UTC(a.getFullYear(), a.getMonth()+1, a.getDate());
+        var msDateB = Date.UTC(b.getFullYear(), b.getMonth()+1, b.getDate());
+
+        if (parseFloat(msDateA) < parseFloat(msDateB))
+            this.latestchat= b; // lt
+        else if (parseFloat(msDateA) == parseFloat(msDateB))
+            this.latestchat= b;  // eq
+        else if (parseFloat(msDateA) > parseFloat(msDateB))
+            this.latestchat= a; // gt
+        else
+            return null;  // error
     }
   }
 
@@ -74,6 +95,7 @@ function parsingGroupConversation(data) {
                     console.log("old user: "+ thisname+", added to: " + currData.bubblecount);
                     ++currData.bubblecount;
                     currData.textcount += splitedLine[2].length;
+                    currData.setLateDateAsLatestChat(currData.latestchat, splitedLine[0]);
                     users.set(thisname, currData);
                     recent_user= currData;  // 마지막 유저
                 }
@@ -82,32 +104,36 @@ function parsingGroupConversation(data) {
                     const thisuser= new MyUser(thisname);
                     ++thisuser.bubblecount;
                     thisuser.textcount += splitedLine[2].length;
+                    thisuser.setFirstChat(splitedLine[0]);
+                    thisuser.latestchat= splitedLine[0];
                     users.set(thisname, thisuser);
                     console.log("new user: " + thisname +", added to: " + thisuser.bubblecount);
                     recent_user= thisuser;  // 마지막 유저
                 }
-                //
-
-                //console.log(users);
             }
             else{
                 currData= users.get(recent_username)
                 currData.textcount += splitedLine[0].length;
                 console.log("connected line, new text lines: "+ splitedLine[0].length +", " + recent_username +"'s total texts count is now: " + currData.textcount);
                 users.set(thisname, currData);
-                
             }
-
         }
+
+        const obj = Object.fromEntries(users);
+        console.log(obj);
+        parsingResult= obj;
+        return parsingResult;
     };
     reader.readAsText(data.file, /* optional */ "euc-kr");
+
+    
 
     //data.days: 일수
     //data.file: 파일내용인데 이걸 읽어서 txt로 반환.
     // 테이블에서 볼 수 있도록 json으로 변환.
     
     // 리턴 텍스트데이터
-    return 'parsingResult';
+    //return parsingResult;
 }
 
 
